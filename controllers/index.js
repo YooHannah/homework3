4fd3dd405e1171f96f98b53066363502ccb0904d
YooHannah <1942063705@qq.com>
2017-09-21 18:07:43 +0800

    finish test

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,22 @@
 var fn_index = async (ctx, next) => {
-    ctx.response.body ={
-              retcode: 0, // 可以是0(正确)，1(登陆台丢失)等等
-              msg: 'success', // 错误时候返回的信息
-              res: `<h1>Index</h1>
+    // ctx.response.body ={
+    //           retcode: 0, // 可以是0(正确)，1(登陆台丢失)等等
+    //           msg: 'success', // 错误时候返回的信息
+    //           res: `<h1>Index</h1>
+    //           <form action="/signin" method="post">
+    //           <p>Name: <input name="name" value="koa"></p>
+    //           <p>Password: <input name="password" type="password"></p>
+    //           <p><input type="submit" value="Submit"></p>
+    //           </form>`
+    //         }
+    ctx.response.body = `<h1>Index</h1>
               <form action="/signin" method="post">
               <p>Name: <input name="name" value="koa"></p>
               <p>Password: <input name="password" type="password"></p>
               <p><input type="submit" value="Submit"></p>
               </form>`
-            }
+    ctx.response.message='success';
+    ctx.response.status= 200
 };
 
 var fn_signin = async (ctx, next) => {
--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 const assert = require('assert')
-// const request = require('request');
+const $ = require('jquery')
 let storage = require('../lib/storage')
 let DB = require('../lib/core')
 let MyDB;
@@ -8,16 +8,13 @@ describe('DB', function () {
         MyDB = class My extends DB {
             constructor(options) {
                 super(options)
-                console.log(1)
                 this.plugin('endpoint', (options) => {
-                   console.log(4)
                     return () => {
                        const request = new XMLHttpRequest();
                         return new Promise((resolve, reject) => {
                         request.onreadystatechange = function () {
                           if (request.readyState === 4) {
                               if (request.status === 200) {
-                                console.log(6.1)
                                 var temp = request.response;
                                 temp=JSON.parse(temp)
                                   resolve({ret:temp.retcode});
@@ -28,7 +25,6 @@ describe('DB', function () {
                       };
                       request.open(options.method, options.url, true)
                       request.send(null)
-                      console.log(5)
                         })
                     }
                 })
@@ -38,7 +34,6 @@ describe('DB', function () {
         const myDB = new MyDB({ ret: 321, url: 'http://localhost:3000/hello/hannah' })
         myDB.get(null, { nocache: true })
             .done((data) => {
-              console.log(9)
                 data.ret.should.equal(1234)
                 done()
             })
@@ -51,8 +46,7 @@ describe('DB', function () {
             .done((data) => {
                 xxxx; // throw an error
             })
-            .catch(e => { //cache.js  plugin resolve 的try...catch 的catch throw e
-               console.log('catch')
+            .catch(e => { //cache.js  plugin resolve 的try...catch 的catch throw e， 在catchHelper的try...catch中fire defer.catCbs
                 e.message.should.equal('DB(http://localhost:3000/hello/hannah) xxxx is not defined')
                 done()
             })
@@ -85,80 +79,259 @@ describe('DB', function () {
             })
     })
 
-    // it('should able to throw error when using jQuery.Defer', (done) => {
-    //     const dDB = new DDB({ ret: 321, url: 'ppp://321' })
+    it('should able to throw error when using jQuery.Defer', (done) => {
+        const dDB = new DDB({ ret: 321, url: 'ppp://321' })
 
-    //     dDB.get(null, { nocache: true })
-    //         .done((data) => {
-    //             xxxx; // throw an error
-    //         }).catch(e => {
-    //             e.message.should.equal('DB(ppp://321) xxxx is not defined')
-    //             done()
-    //         })
-    // })
+        dDB.get(null, { nocache: true })
+            .done((data) => {
+                xxxx; // throw an error
+            }).catch(e => {
+                e.message.should.equal('DB(ppp://321) xxxx is not defined')
+                done()
+            })
+    })
 
-    // it('should able to use judge', (done) => {
-    //     class XDB extends DB {
-    //         constructor(options) {
-    //             super(options)
-    //             this.plugin('endpoint', () => {
-    //                 return () => {
-    //                     return new Promise((resolve, reject) => {
-    //                         setTimeout(() => {
-    //                             resolve({ hasError: true })
-    //                         })
-    //                     })
-    //                 }
-    //             })
-    //             this.plugin('judge', (data) => {
-    //                 if (data.hasError) return false
-    //                 return true
-    //             })
-    //         }
-    //     }
+    it('should able to use judge', (done) => {
+        class XDB extends DB {
+            constructor(options) {
+                super(options)
+                this.plugin('endpoint', () => {
+                    return () => {
+                        return new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                                resolve({ hasError: true })
+                            })
+                        })
+                    }
+                })
+                this.plugin('judge', (data) => {
+                    if (data.hasError) return false
+                    return true
+                })
+            }
+        }
 
-    //     const xDB = new XDB
+        const xDB = new XDB
 
-    //     xDB.get()
-    //         .fail(e => {
-    //             e.hasError.should.ok()
-    //             done()
-    //         })
-    // })
+        xDB.get()
+            .fail(e => {
+                e.hasError.should.ok()
+                done()
+            })
+    })
 
-    // it('should able to use error plugin', (done) => {
-    //     class XDB extends DB {
-    //         constructor(options) {
-    //             super(options)
-    //             this.plugin('endpoint', () => {
-    //                 return () => {
-    //                     return new Promise((resolve, reject) => {
-    //                         setTimeout(() => {
-    //                             resolve({ hasError: true })
-    //                         })
-    //                     })
-    //                 }
-    //             })
-    //             this.plugin('judge', (data) => {
-    //                 if (data.hasError) return false
-    //                 return true
-    //             })
-    //             this.plugin('error', (data) => {
-    //                 return {
-    //                     data
-    //                 }
-    //             })
-    //         }
-    //     }
+    it('should able to use error plugin', (done) => {
+        class XDB extends DB {
+            constructor(options) {
+                super(options)
+                this.plugin('endpoint', () => {
+                    return () => {
+                        return new Promise((resolve, reject) => {
+                            setTimeout(() => {
+                                resolve({ hasError: true })
+                            })
+                        })
+                    }
+                })
+                this.plugin('judge', (data) => {
+                    if (data.hasError) return false
+                    return true
+                })
+                this.plugin('error', (data) => {
+                    return {
+                        data
+                    }
+                })
+            }
+        }
 
-    //     const xDB = new XDB
+        const xDB = new XDB
 
-    //     xDB.get()
-    //         .fail(e => {
-    //             e.data.hasError.should.ok()
-    //             done()
-    //         })
-    // })
+        xDB.get()
+            .fail(e => {
+                e.data.hasError.should.ok()
+                done()
+            })
+    })
+    })
+
+    describe('defer error', () => {
+        let db
+        before(() => {
+            window._ENV_ = 'dev'
+        })
+
+        it('should throw an error when have not use fail method to listen the error', (done) => {
+            class XDB extends DB {
+                constructor(options) {
+                    super(options)
+                    this.plugin('endpoint', () => {
+                        return () => {
+                            return new Promise((resolve, reject) => {
+                                setTimeout(() => {
+                                    resolve({ hasError: true })
+                                })
+                            })
+                        }
+                    })
+                    this.plugin('judge', (data) => {
+                        if (data.hasError) return false
+                        return true
+                    })
+                }
+            }
+
+            const xDB = new XDB({ url: 'kk://123' })
+
+            xDB.get({ nocache: true })
+                .catch((e) => {
+                    e.message.should.equal('You need use fail method to get the error: {"hasError":true}')
+                    done()
+                })
+        })
+
+        after(() => {
+            window._ENV_ = undefined
+            delete window._ENV_
+        })
+    })
 
 
-})
+    describe('cache default', () => {
+        let db
+        before((done) => {
+            db = new MyDB({ ret: 123, url: 'http://localhost:3000/hello/hannah' })
+            db.get()
+                .done((data) => {
+                    data.ret.should.equal(1234)
+                    done()
+                })
+        })
+
+        it('should able to use cache', (done) => {
+            db.get()
+                .done((data, flag) => {
+                    if (flag) {
+                        flag.should.equal('CACHE')
+                        data.ret.should.equal(1234)
+                        done()
+                    } else {
+                        data.ret.should.equal(1234)
+                    }
+                })
+        })
+
+        after((done) => {
+            setTimeout(() => {
+                localStorage.clear()
+                done()
+            }, 100)
+        })
+    })
+
+     describe('cache lazy', () => {
+        let db
+        before((done) => {
+            db = new MyDB({ ret: 123, url: 'http://localhost:3000/hello/hannah' })
+            db.get()
+                .done((data) => {
+                    data.ret.should.equal(1234)
+                    done()
+                })
+        })
+
+        it('should able to use cache', (done) => {
+            db.get(null, { lazy: true })
+                .done((data, flag) => {
+                    flag.should.equal('CACHE')
+                    data.ret.should.equal(1234)
+                    done()
+                })
+        })
+
+        after((done) => {
+            setTimeout(() => {
+                localStorage.clear()
+                done()
+            }, 100)
+        })
+    })
+
+    describe('cache refresh', () => {
+        let db
+        before((done) => {
+            db = new MyDB({ ret: 123, url: 'http://localhost:3000/hello/hannah' })
+            db.get()
+                .done((data) => {
+                    data.ret.should.equal(1234)
+                    done()
+                })
+        })
+
+        it('should able to use cache', (done) => {
+            db.get(null, { refresh: 10000 })
+                .done((data, flag) => {
+                    flag.should.equal('CACHE')
+                    data.ret.should.equal(1234)
+                    done()
+                })
+        })
+
+        it('should able to use cache', (done) => {
+            db.get(null, { refresh: 10 })
+                .done((data, flag) => {
+                    if (flag) {
+                        flag.should.equal('CACHE')
+                        data.ret.should.equal(1234)
+                        done()
+                    } else {
+                        data.ret.should.equal(1234)
+                    }
+                })
+        })
+
+        after((done) => {
+            setTimeout(() => {
+                localStorage.clear()
+                done()
+            }, 100)
+        })
+    })
+
+     describe('cache lazy & refresh', () => {
+        let db
+        before((done) => {
+            db = new MyDB({ ret: 123, url: 'http://localhost:3000/hello/hannah' })
+            db.get()
+                .done((data) => {
+                    data.ret.should.equal(1234)
+                    done()
+
+                })
+        })
+
+        it('should able to use cache', (done) => {
+            db.get(null, { lazy: true, refresh: 10000 })
+                .done((data, flag) => {
+                    flag.should.equal('CACHE')
+                    data.ret.should.equal(1234)
+                    done()
+                })
+        })
+
+        it('should able to use cache', (done) => {
+            db.get(null, { lazy: true, refresh: 1 })
+                .done((data, flag) => {
+                    (flag === undefined).should.be.ok()
+                    data.ret.should.equal(1234)
+                    done()
+                })
+        })
+
+        after((done) => {
+            setTimeout(() => {
+                localStorage.clear()
+                done()
+            }, 100)
+        })
+    })
